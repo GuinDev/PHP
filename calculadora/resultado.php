bc06849096ba4a6afcbb10a9f708b69b0960902d
GuinDev <guineveresiqaf@gmail.com>
2023-09-10 16:13:11 -0300

    Reformated resultado.php

--- calculadora/resultado.php
+++ calculadora/resultado.php
@@ -26,7 +26,7 @@ elseif($op == "/")
 <meta http-equiv="refresh" content="15"/>
 <title>Calculadora</title>
 </head>
-<!-- 00 == 0 ff==255  ffffff == red255,green255,blue255 -->
+<!--00 == 0 ff==255  ffffff == red255,green255,blue255-->
 <body  style="background-color: #00b1ff; color: white;">
   <h1>Calculadora</h1>
     <form action="resultado.php" method="GET">
@@ -36,8 +36,9 @@ elseif($op == "/")
       <input type="submit" value="Calcular">
     </form>     
       <p> o resultado eh: <?php
-    echo "$resul";
-    ?> </p>
+      echo "$resul";
+      ?> 
+      </p>
 </body>
 </html>
 
